3147f3ecf4851ba3d1ebc15d81df84f9002eda36
Pawel <hyzy1988@o2.pl>
2020-09-26 13:59:52 +0200

    [change] - depedency injcetcions exercises

--- src/main/java/pl/sda/hellospring/injection/MyService.java
+++ src/main/java/pl/sda/hellospring/injection/MyService.java
@@ -2,13 +2,14 @@ package pl.sda.hellospring.injection;
 
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class MyService {
-    public MyService() {
+    public final MyRepo myRepo;
+    public MyService(MyRepo myRepo) {
+        this.myRepo = myRepo;
         log.info("pl.sda.hellospring.injection.MyService");
     }
 }
