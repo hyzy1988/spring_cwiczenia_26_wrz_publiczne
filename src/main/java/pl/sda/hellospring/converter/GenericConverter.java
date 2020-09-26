ce88ac88576d24aa1dcc1d614342054932e53eec
Pawel <hyzy1988@o2.pl>
2020-09-26 15:48:20 +0200

    [add] - converters implemented

---
+++ src/main/java/pl/sda/hellospring/converter/GenericConverter.java
@@ -0,0 +1,10 @@
+package pl.sda.hellospring.converter;
+
+public interface GenericConverter<R, T> {
+
+    T fromEntityToDto (R entity);
+    R fromDtoToEntity (T dto);
+
+
+
+}
--- src/main/java/pl/sda/hellospring/dto/CarDto.java
+++ src/main/java/pl/sda/hellospring/dto/CarDto.java
@@ -1,9 +1,11 @@
 package pl.sda.hellospring.dto;
 
 
+import lombok.Builder;
 import lombok.Value;
 
 @Value
+@Builder
 public class CarDto {
     private String brand;
     private String model;
